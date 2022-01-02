a3948f6f70a7ff54b57d207d2cc2ba2d88ffaca1
정세욱 <jeongse-ug@jeongse-ugs-MacBook-Air.local>
2022-01-02 13:31:21 +0900

    [MODIFY] Changed User login view class naming

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserListView, UserLoginView, UserSignUpView
+from .views import UserListView, UserSignInView, UserSignUpView
 
 
 urlpatterns = [
     path("user", UserListView.as_view()),
-    path("user/login", UserLoginView.as_view()),
+    path("user/sign-in", UserSignInView.as_view()),
     path("user/sign-up", UserSignUpView.as_view()),
 ]
