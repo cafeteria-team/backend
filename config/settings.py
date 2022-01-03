73d65ff56bb1516eca756369361a3047d3341bba
정세욱 <tpdnrqkqh@gmail.com>
2022-01-03 23:04:46 +0900

    [MODIFY] Database settings

--- config/settings.py
+++ config/settings.py
@@ -86,14 +86,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "goodfeteria",
+#         "USER": "admin",
+#         "PASSWORD": "1q2w3e4r##",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "goodfeteria",
-        "USER": "admin",
-        "PASSWORD": "1q2w3e4r##",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
 
