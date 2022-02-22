2a37608f17f3782eb9fe4438d756e98d0998e1e0
정세욱 <tpdnrqkqh@gmail.com>
2022-02-23 02:57:20 +0900

    [ADD] User detail serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from .models import User
 from store.models import Store
-from store.serializers import MemberStoreSerializer
+from store.serializers import MemberStoreSerializer, MemberDetailStoreSerialzer
 
-from core.exceptions.exceptions import DuplicationException
+from core.exceptions.exceptions import DuplicationException, ValidateException
+
+from .models import User
+from .manager import UserMananger
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -88,12 +90,12 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         try:
             busi_num = int(busi_num)
         except:
-            msg = "사업자 번호는 숫자만 사용할 수 있습니다."
+            msg = {"msg": "사업자 번호는 숫자만 사용할 수 있습니다."}
             raise ValidationError(msg)
 
         if store.exists():
             store = store.first()
-            msg = "사업자 번호가 이미 존재합니다. 확인 후 다시 시도해주세요."
+            msg = {"msg": "사업자 번호가 이미 존재합니다. 확인 후 다시 시도해주세요."}
             raise DuplicationException(msg)
 
         try:
@@ -133,3 +135,39 @@ class UserRegisterResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["username"]
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    store = MemberDetailStoreSerialzer()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "store"]
+
+
+class UserDetailUpdateSerializer(serializers.ModelSerializer):
+    store = MemberDetailStoreSerialzer()
+
+    class Meta:
+        model = User
+        fields = ["email", "store"]
+
+    def update(self, instance, validated_data):
+        user = User.objects.filter(email=validated_data["email"])
+        if user.exists():
+            msg = "해당 이메일은 이미 중복입니다. 확인 후 다시 시도해주세요"
+            raise DuplicationException(msg)
+
+        type_check_busi_num = UserMananger.type_check_busi_num(
+            busi_num=validated_data["store"]["busi_num"]
+        )
+
+        if not type_check_busi_num:
+            msg = "사업자 번호는 숫자만 사용할 수 있습니다."
+            raise ValidateException(msg)
+
+        instance.email = validated_data["email"]
+        instance.store.name = validated_data["store"]["name"]
+        instance.store.busi_num = validated_data["store"]["busi_num"]
+        instance.save()
+        return instance
