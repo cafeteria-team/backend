820f3d942698faa0e10d099b8fe088624d54b3b9
정세욱 <tpdnrqkqh@gmail.com>
2022-02-28 00:55:59 +0900

    [ADD] Refresh token url

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path
+
+
 from .views import (
     UserListView,
     UserSignInView,
     UserLogoutView,
     UserRegisterView,
     UserDetailView,
+    CustomUserRefreshTokenView,
 )
 
 
 urlpatterns = [
-    path("user", UserListView.as_view()),
-    path("user/login", UserSignInView.as_view()),
-    path("user/register", UserRegisterView.as_view()),
-    path("user/logout", UserLogoutView.as_view()),
-    path("user/<int:pk>", UserDetailView.as_view()),
+    path("user", UserListView.as_view(), name="user_lists"),
+    path("user/login", UserSignInView.as_view(), name="user_login"),
+    path("user/register", UserRegisterView.as_view(), name="user_register"),
+    path("user/logout", UserLogoutView.as_view(), name="user_logout"),
+    path("user/<int:pk>", UserDetailView.as_view(), name="user_detail"),
+    path(
+        "user/token/refresh/",
+        CustomUserRefreshTokenView.as_view(),
+        name="token_refresh",
+    ),
 ]
