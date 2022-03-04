c1725483024e7e042fe479b2e180b5dc77cfdee9
정세욱 <tpdnrqkqh@gmail.com>
2022-03-04 22:17:20 +0900

    [MODIFY] Changed sign in to login name of class

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from .views import (
     UserListView,
-    UserSignInView,
+    UserLoginView,
     UserLogoutView,
     UserRegisterView,
     UserDetailView,
@@ -13,7 +13,7 @@ from .views import (
 
 urlpatterns = [
     path("user", UserListView.as_view(), name="user_lists"),
-    path("user/login", UserSignInView.as_view(), name="user_login"),
+    path("user/login", UserLoginView.as_view(), name="user_login"),
     path("user/register", UserRegisterView.as_view(), name="user_register"),
     path("user/logout", UserLogoutView.as_view(), name="user_logout"),
     path("user/<int:pk>", UserDetailView.as_view(), name="user_detail"),
