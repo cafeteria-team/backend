ace1129f70829de5dc8886367e4b37c09cc5312c
정세욱 <tpdnrqkqh@gmail.com>
2022-03-11 00:56:31 +0900

    [ADD] This file will management messages of phone app

---
+++ phone/messages.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class PhoneMessages:
+    SMS_SEND_ERROR = "SMS 인증번호 발송에 실패했습니다.\n관리자에게 문의하세요."
+    SMS_AUTH_COMMENT = "[좋구내] 인증번호는 [{}]입니다. 타인 노출 금지"
