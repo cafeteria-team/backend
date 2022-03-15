749a221385489976f2d52304377066d458e621cb
정세욱 <tpdnrqkqh@gmail.com>
2022-03-16 01:33:58 +0900

    [ADD] Definition postgresql database information

--- config/settings/base.py
+++ config/settings/base.py
@@ -151,21 +151,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "goodfeteria",
-#         "USER": "admin",
-#         "PASSWORD": "1q2w3e4r##",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "backend",
+        "USER": "backend",
+        "PASSWORD": "backend",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
