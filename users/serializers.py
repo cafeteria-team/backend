ba7f9e8acd6b02497e8fffcfdef2e16351e390a1
정세욱 <tpdnrqkqh@gmail.com>
2022-03-21 00:32:11 +0900

    [ADD] Password field of user update seirlaizer

--- users/serializers.py
+++ users/serializers.py
@@ -158,10 +158,11 @@ class UserDetailUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["email", "store"]
+        fields = ["email", "password", "store"]
 
     def update(self, instance, validated_data):
         user = User.objects.filter(email=validated_data["email"])
+
         if user.exists():
             msg = "해당 이메일은 이미 중복입니다. 확인 후 다시 시도해주세요"
             raise DuplicationException(msg)
@@ -177,6 +178,7 @@ class UserDetailUpdateSerializer(serializers.ModelSerializer):
         instance.email = validated_data["email"]
         instance.store.name = validated_data["store"]["name"]
         instance.store.busi_num = validated_data["store"]["busi_num"]
+        instance.set_password(validated_data["password"])
         instance.save()
         return instance
 
