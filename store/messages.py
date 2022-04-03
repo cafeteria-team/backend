1bf824ef76fed84513083bf16340625f5065b801
정세욱 <tpdnrqkqh@gmail.com>
2022-04-03 23:59:46 +0900

    [MODIFY] Removed confict error message and added deleted success

--- store/messages.py
+++ store/messages.py
@@ -1,6 +1,6 @@
 class FacilityMessages:
     STORE_ID_PARAM_ERROR = "STORE ID 파라미터는 필수입니다."
-    FACILITY_CONFICT_ERROR = "한번에 동일한 편의시설을 등록할 수 없습니다. {} 중복되게 입력하지 마세요."
     GET_FACILITY_ERROR = "편의시설 및 서비스 정보를 가지고 오는데 실패했습니다."
-    ALREADY_EXISTS_ERROR = "{}는(은) 이미 등록되어 있는 편의시설입니다. {}를(을) 제거해주세요."
+    ALREADY_EXISTS_ERROR = "[{}] 카테고리로 {}는(은) 이미 등록되어 있는 편의시설입니다."
     CREAT_SUCCESS = "편의시설이 정상적으로 등록되었습니다."
+    DELETE_SUCCESS = "편의시설이 정상적으로 삭제되었습니다."
