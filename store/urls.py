3e8b99b59fa1d6cf13722938048796c4b0121a63
정세욱 <tpdnrqkqh@gmail.com>
2022-04-18 00:42:47 +0900

    [ADD] User facility urls and generic views

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,31 @@
 from django.urls import path
 
-from .views import FacilityAdminView, FacilityAdminDetailView
+from .views import (
+    FacilityAdminView,
+    FacilityAdminDetailView,
+    FacilityJoinView,
+    FacilityJoinDetailView,
+)
 
 urlpatterns = [
-    path("facility", FacilityAdminView.as_view(), name="facility_admin"),
+    path(
+        "facility",
+        FacilityAdminView.as_view(),
+        name="facility_admin",
+    ),
     path(
         "facility/<int:facility_id>",
         FacilityAdminDetailView.as_view(),
         name="facility_admin_detail",
+    ),
+    path(
+        "facility/join/<int:store_id>",
+        FacilityJoinView.as_view(),
+        name="facility_join",
+    ),
+    path(
+        "facility/join/<int:store_id>/<int:join_facility_id>",
+        FacilityJoinDetailView.as_view(),
+        name="facility_join",
     ),
 ]
