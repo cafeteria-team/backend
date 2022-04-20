0771281ad7eb549a984d2443706c724be4ff102e
정세욱 <tpdnrqkqh@gmail.com>
2022-04-21 00:30:54 +0900

    [ADD] Summary of menu methods

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
+
 from rest_framework import generics, status, mixins
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 
 from core.exceptions.exceptions import ValidateException
 from core.pagination.pagination import CustomPagination
@@ -20,9 +22,11 @@ class MenuView(generics.ListCreateAPIView):
         elif self.request.method == "POST":
             return MenuCreateSerializer
 
+    @swagger_auto_schema(operation_summary="메뉴 리스트 조회(*)")
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
+    @swagger_auto_schema(operation_summary="메뉴 생성(*)")
     def post(self, request, *args, **kwargs):
         store_id = kwargs.get("store_id", None)
 
@@ -44,9 +48,11 @@ class MenuDetailView(
     lookup_field = "id"
     lookup_url_kwarg = "menu_id"
 
+    @swagger_auto_schema(operation_summary="메뉴 상세 수정(*)")
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
+    @swagger_auto_schema(operation_summary="메뉴 삭제(*)")
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
@@ -66,5 +72,6 @@ class MenuTodayListView(generics.ListAPIView):
         )
         return queryset
 
+    @swagger_auto_schema(operation_summary="메뉴 리스트(오늘)(*)")
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
